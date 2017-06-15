868aad08706440a5306b87dc29921195fb6491fc
Gavin Staniforth <gavin@usemarkup.com>
2017-06-15 23:57:28 +0100

    chore: init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # chef-phabricator
 A knife solo project for deploying a phabricator instance
+
+# Setup
+
+(Install Ruby >=2.2)
+```bash
+make install
+```
+
+# Run the tests
+
+```bash
+make bundle
+make kitchen
+```
+
---
+++ chef/metadata.rb
@@ -0,0 +1,11 @@
+name             'phabricator'
+license          'MIT Licence'
+description      'phabricator project'
+version          '0.0.1'
+
+depends 'packages', '~> 1'
+depends 'yum-epel', '~> 2.1'
+depends 'yum-ius', '~> 2.2'
+depends 'chef_nginx', '~> 6.1'
+depends 'php', '~> 4.2'
+depends 'php-fpm', '~> 0.7'
