0404dc8fb6ccddff1a44b65916748c1165942ab5
Gavin Staniforth <gavin@usemarkup.com>
2017-06-16 01:17:30 +0100

    chore: add more inspec tests

--- chef/tests/nginx.rb
+++ chef/tests/nginx.rb
@@ -1,3 +1,19 @@
+describe yum.repo('nginx') do
+  it { should exist }
+  it { should be_enabled }
+end
+
+describe package('nginx') do
+  it { should be_installed }
+  its('version') { should >= '1.12.0' }
+end
+
+describe service('nginx') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
 describe port(80) do
   it { should be_listening }
   its('protocols') {should include 'tcp'}
