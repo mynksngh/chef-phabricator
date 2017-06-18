e5d7a211db42d8a6431f68d9a1472b655babb854
Gavin Staniforth <gavin@usemarkup.com>
2017-06-18 04:16:05 +0100

    chore: wip

---
+++ chef/site-cookbooks/phabricator/metadata.rb
@@ -0,0 +1,4 @@
+name              "phabricator"
+maintainer        "Markup"
+description       ""
+version           "0.0.1"
---
+++ chef/site-cookbooks/phabricator/recipes/default.rb
@@ -0,0 +1,16 @@
+deploy "phabricator" do
+  deploy_to '/srv/phabricator'
+  repo 'https://github.com/gsdevme/phabricator-source.git'
+  revision 'master'
+  action :deploy
+
+  rollback_on_error true
+
+  migration_command 'make'
+  migrate true
+
+  symlink_before_migrate ({})
+  create_dirs_before_symlink []
+  purge_before_symlink []
+  symlinks ({})
+end
